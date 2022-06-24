ff125271ab9cf172b9da66a6e3e5f0865b77eda8
Akmalpm <95691962+Akmalpm@users.noreply.github.com>
2022-06-24 17:08:51 +0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,74 @@
+//menu
+var tombolMenu = $(".tombol-menu");
+var menu = $("nav .menu ul");
+
+function klikMenu() {
+    tombolMenu.click(function () {
+        menu.toggle();
+    });
+    menu.click(function () {
+        menu.toggle();
+    });
+}
+
+$(document).ready(function () {
+    var width = $(window).width();
+    if (width < 990) {
+        klikMenu();
+    }
+})
+
+//check lebar
+$(window).resize(function () {
+    var width = $(window).width();
+    if (width > 989) {
+        menu.css("display", "block");
+        //display:block
+    } else {
+        menu.css("display", "none");
+    }
+    klikMenu();
+});
+
+//efek scroll 
+$(document).ready(function () {
+    var scroll_pos = 0;
+    $(document).scroll(function () {
+        scroll_pos = $(this).scrollTop();
+        if (scroll_pos > 0) {
+            $("nav").addClass("putih");
+            $("nav img.hitam").show();
+            $("nav img.putih").hide();
+        } else {
+            $("nav").removeClass("putih");
+            $("nav img.hitam").hide();
+            $("nav img.putih").show();
+        }
+    })
+});
+
+
+
+// efek loading search
+
+var search = document.getElementById('search');
+var input = document.getElementById('input');
+var button = document.getElementById('button');
+
+function loading(){
+    search.classList.add('loading');
+
+    setTimeout(function() {
+        search.classList.remove('loading');
+    }, 1500);
+
+   
+
+
+}
+
+button:addEventListener('click', loading);
+input.addEventListener('keydown', function() {
+    if (event.keyCode == 13) loading();
+})
+ 
